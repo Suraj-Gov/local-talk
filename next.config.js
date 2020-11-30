43d4bd71ea3e0d91e4e7833613c94b57b2402da5
Suraj-Gov <surajgovind332000@gmail.com>
2020-12-01 02:51:53 +0530

    removed pg_native plugin ignore

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 module.exports = {
   target: "serverless",
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
-    config.plugins.push(new webpack.IgnorePlugin(/^pg-native$/));
-    return config;
-  },
 };
